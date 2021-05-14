c10c4c22aec31a2719a92fb8fc5e5958e71fd46c
Ryan Shew <adoubtfullogic@gmail.com>
2021-05-13 21:44:19 -0700

    Day 55 - pt2 - Advanced decorators and Flask

--- day-55/server.py
+++ day-55/server.py
@@ -1,5 +1,10 @@
 from flask import Flask
+import random
+
+number = random.randint(0,9)
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello():
@@ -7,7 +12,14 @@ def hello():
         '<img src="https://media.giphy.com/media/BzyTuYCmvSORqs1ABM/giphy.gif" width=200>'
 
 
-
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess < number:
+        return "Your guess is too low."
+    elif guess > number:
+        return "Your guess is too high."
+    else:
+        return "Your guess is correct."
 
 
 if __name__ == "__main__":
